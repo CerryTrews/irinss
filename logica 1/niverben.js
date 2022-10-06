0a8f695f47085213350bec6cde4112c1ae80e245
CerryTrews <114422574+CerryTrews@users.noreply.github.com>
2022-10-06 19:36:33 -0300

    Delete logica 1 directory

--- logica 1/niverben.js
+++
@@ -1,7 +0,0 @@
-let convidadosAdultos = ['gerson', 'eliziane', 'marcia', 'frank', 'bruna', 'tati', 'andre', 'masson', 'sayuri', 'afonso', 'julia', 'neir', 'diva', 'rodrigo', 'denise', 'gustavo', 'gustavo', 'denis', 'le'];
-let convidadosCriancas = [ 'marina', 'murilo', 'miguel', 'catarina', 'pedro', 'bento', 'clara'];
-
-console.log(`Adultos ${convidadosAdultos.length}`)
-console.log(`Crianças ${convidadosCriancas.length}`)
-let soma = convidadosAdultos.length + convidadosCriancas.length;
-console.log(soma)
